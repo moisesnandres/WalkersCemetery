47feb6de4bf5d83c324b2ec53f25414c6d924152
Moisés Ñañez <moisesnandres@gmail.com>
2016-05-19 23:34:56 -0500

    Use haml and add js for close alert

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
   $('.fa-bars').click(function(){
     $('nav ul').slideToggle('slow');
   });
+  $('.alert-message i').each(function(){
+    $(this).click(function(){
+      $(this).parent().parent().remove();
+    });
+  });
 });
 $(window).resize(function(){
   if ($(window).width()>768) {
