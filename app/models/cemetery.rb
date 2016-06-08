b1d1109be52802fc6501b17a0a113f5ff68ac22e
Jose Mitacc <jmitacc0128@gmail.com>
2016-06-07 22:35:13 -0500

    Agregar tabla de cementerio

---
+++ app/models/cemetery.rb
@@ -0,0 +1,2 @@
+class Cemetery < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525052237) do
+ActiveRecord::Schema.define(version: 20160608033244) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,5 +34,13 @@ ActiveRecord::Schema.define(version: 20160525052237) do
   add_index "admins", ["confirmation_token"], name: "index_admins_on_confirmation_token", unique: true
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "cemeteries", force: :cascade do |t|
+    t.string   "nombre"
+    t.string   "direccion"
+    t.string   "telefono"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
