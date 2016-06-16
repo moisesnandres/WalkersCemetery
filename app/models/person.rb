25a9029e5bc094ede29e5beabce59558c30c7881
Moisés Ñañez <moisesnandres@gmail.com>
2016-06-15 22:05:40 -0500

    Fix people view

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,18 +5,13 @@ class Person < ActiveRecord::Base
     ],
   )
   belongs_to :sector
+
   scope :search_query, lambda { |query|
     return nil  if query.blank?
-    # condition query, parse into individual keywords
     terms = query.downcase.split(/\s+/)
-    # replace "*" with "%" for wildcard searches,
-    # append '%', remove duplicate '%'s
     terms = terms.map { |e|
       (e.gsub('*', '%') + '%').gsub(/%+/, '%')
     }
-    # configure number of OR conditions for provision
-    # of interpolation arguments. Adjust this if you
-    # change the number of OR conditions.
     num_or_conds = 2
     where(
       terms.map { |term|
@@ -25,4 +20,9 @@ class Person < ActiveRecord::Base
       *terms.map { |e| [e] * num_or_conds }.flatten
     )
   }
+
+  def nombres_completos
+    [nombres, apellidos].join(' ')
+  end
+
 end
